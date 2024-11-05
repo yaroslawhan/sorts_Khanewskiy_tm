aff67a7a2b6365a5ea9b0219fafbebea8dcd6022
polumba365 <plnmlcnk@gmail.com>
2024-11-05 19:23:26 +0300

    first commit

---
+++ inserts_by_Omelchenko.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+string inserts_by_Omelchenko(string str) {
+    // Разбиваем строку на слова
+    stringstream ss(str);
+    string word;
+    vector<string> words;
+    while (getline(ss, word, ' ')) {
+        words.push_back(word);
+    }
+
+    // Сортировка вставкой
+    int n = words.size();
+    for (int i = 1; i < n; ++i) {
+        string key = words[i];
+        int j = i - 1;
+
+        // Сдвиг элементов, больших, чем ключ, вправо
+        while (j >= 0 && words[j] > key) {
+            words[j + 1] = words[j];
+            j--;
+        }
+
+        // Вставка ключа в правильную позицию
+        words[j + 1] = key;
+    }
+
+    // Собираем строку обратно
+    stringstream result;
+    for (const auto& word : words) {
+        result << word << " ";
+    }
+    return result.str();
+}
+
+
