83d8b8c9e01d1d63d30f9ad74a37e1b5a751d881
DarkeEnergyy <ezhovnikita2005@gmail.com>
2024-12-15 17:39:21 +0300

    BICHik

--- sort_Khanewskiy_tm/ABCsort_by_Ezhov.cpp
+++ sort_Khanewskiy_tm/ABCsort_by_Ezhov.cpp
@@ -1,120 +1,91 @@
 #include "sort_algorithms.h"
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstring>
+#include <sstream>
 
-#define alphamin 'a'  // Минимальный символ алфавита (строчные буквы)
-#define alphamax 'z'  // Максимальный символ алфавита (строчные буквы)
-#define N 5       // Количество слов для сортировки
-#define noduplicates 0 // Флаг для удаления дубликатов (1 - удалять, 0 - не удалять)
-#define MAX_KEYS 1000    // Максимальное количество ключевых полей
+constexpr char alphamin = 'a';
+constexpr char alphamax = 'z';
 
-char data[N][50];  // Массив строк для сортировки (с максимальной длиной строки 50 символов)
-long sorted_records[N];  // Массив для отсортированных записей
-long RT[N];  // Таблица записей
-long LT[MAX_KEYS + 1][alphamax + 1];  // Таблица символов
-
-// Получение следующего символа в i-м разряде слова
-char GetNextField(long record_no, int level) {
-    return data[record_no][level - 1];
+char GetNextField(const std::vector<std::string>& data, long record_no, int level) {
+    if (level - 1 < data[record_no].size())
+        return data[record_no][level - 1];
+    return '\0';
 }
 
-// Обработка текущей записи
-void PutCurrRecord(long record_no) {
-    static int index = 0;  // Указатель на текущую позицию
+void PutCurrRecord(std::vector<long>& sorted_records, long record_no, int& index) {
     sorted_records[index++] = record_no;
 }
 
-// Функция обработки данных после 1-го этапа
-void process(int level, int keys) {
+void process(std::vector<long>& RT, std::vector<std::vector<long>>& LT, std::vector<long>& sorted_records,
+    const std::vector<std::string>& data, int level, int keys, int& index) {
     for (char i = alphamin; i <= alphamax; ++i) {
         long recno = LT[level][i];
         LT[level][i] = 0;
 
         while (recno != 0) {
             if (RT[recno] == 0) {
-                PutCurrRecord(recno);
+                PutCurrRecord(sorted_records, recno, index);
                 recno = 0;
                 continue;
             }
             else {
                 if (level == keys) {
                     while (recno != 0) {
-                        PutCurrRecord(recno);
+                        PutCurrRecord(sorted_records, recno, index);
                         recno = RT[recno];
-                        if (noduplicates) recno = 0;
                     }
                 }
                 else {
                     int newlevel = level + 1;
                     while (recno != 0) {
                         long nextrec = RT[recno];
-                        char c = GetNextField(recno, newlevel);
+                        char c = GetNextField(data, recno, newlevel);
                         RT[recno] = LT[newlevel][c];
                         LT[newlevel][c] = recno;
                         recno = nextrec;
                     }
-                    process(newlevel, keys);
+                    process(RT, LT, sorted_records, data, newlevel, keys, index);
                 }
             }
         }
     }
 }
 
-void ABCsort(int keys) {
-    long start, step, stop;
+std::string ABCsort(const std::string& input, int keys = 10) {
+    std::vector<std::string> data;
+    std::vector<long> sorted_records;
+    std::vector<long> RT;
+    std::vector<std::vector<long>> LT(keys + 1, std::vector<long>(alphamax + 1, 0));
 
-    // Инициализация таблицы символов
-    for (int i = 0; i <= keys; ++i) {
-        for (int j = 0; j <= alphamax; ++j) {
-            LT[i][j] = 0;
-        }
+    std::string word;
+    std::istringstream stream(input);
+    data.push_back("");
+    while (stream >> word) {
+        data.push_back(word);
     }
 
-    // Инициализация таблицы записей
-    for (int i = 0; i < N; ++i) {
-        RT[i] = 0;
-    }
+    int N = data.size();
+    sorted_records.resize(N, 0);
+    RT.resize(N, 0);
 
-    if ((keys & 1) ^ noduplicates) {
-        start = N;
-        stop = -1;
-        step = -1;
-    }
-    else {
-        start = 0;
-        stop = N + 1;
-        step = 1;
-    }
-
-    // Первоначальное распределение записей по первому символу
-    for (long recno = start; recno != stop; recno += step) {
-        char c = GetNextField(recno, 1);
+    for (int recno = 0; recno < N; ++recno) {
+        char c = GetNextField(data, recno, 1);
         RT[recno] = LT[1][c];
         LT[1][c] = recno;
     }
 
-    process(1, keys);
-}
+    int index = 0;
+    process(RT, LT, sorted_records, data, 1, keys, index);
 
-int main() {
-    // Заполняем массив данных (например, можно загрузить строки или задать их вручную)
-    for (int i = 1; i < N + 1; ++i) {
-        std::string word;
-        std::cin >> word;
-        for (int j = 0; j < word.length(); ++j) {
-            data[i][j] = word[j];
-        }
-        data[i][word.length()] = '\0';  // Завершающий нулевой символ
-    }
-
-    // Вызываем сортировку (используем, например, 3 ключевых поля)
-    ABCsort(MAX_KEYS);
-
-    // Вывод отсортированных данных
-    std::cout << "Отсортированные данные:\n";
+    std::ostringstream result;
     for (int i = 0; i < N; ++i) {
-        std::cout << data[sorted_records[i]] << std::endl;
+        result << data[sorted_records[i]];
+        if (i < N - 1) {
+            result << " ";
+        }
     }
 
-    return 0;
+    return result.str();
 }
